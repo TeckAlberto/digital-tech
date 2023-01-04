14b921bef0421b922849fb37cadfc2ccc2a51c73
Alberto <jesus.gutierrez8614@alumnos.udg.mx>
2023-01-04 00:00:51 -0600

    First Commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Digital Tech</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body class="antialiased">
+        <h1 class="font-bold text-blue-600">Hola Mundo</h1>
+    </body>
+</html>
